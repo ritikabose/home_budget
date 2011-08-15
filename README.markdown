d8091304f12ddf502bdf62458b862c0cb9f84b61
ritikabose <r3majumdar@yahoo.com>
2011-08-15 15:58:36 -0400

    initial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Home Budget Application
+
+This is the Home Budget application , In which we will calculate our monthly home expensee.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeBudget::Application.config.secret_token = '547820fefb17d327b36ca126e2e97538cda029095d9c61bdd1e98a5252730831ae7b480c21ccfcefc635d0efc0791de75662c47f069e551ea57c886ef281a2d0'
