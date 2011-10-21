104bb8eab8d2db2550f4f9747c0034f2f1c39379
ritika bose <r3majumdar@yahoo.com>
2011-10-21 17:05:15 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 HomeBudget::Application.routes.draw do
-  #get "pages/home"
-
- # get "pages/contact"
- # 
- # get "pages/about"
-  
-  #get "pages/help"
   
   
+  
+  get "users/new"
+ match 'signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
-    match '/about', :to => 'pages#about'
-      match '/help', :to => 'pages#help'
-        root :to => 'pages#home'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  root :to => "pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
