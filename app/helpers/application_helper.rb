607e055ddd2e206adeb04d6873f0ff9eb8dc6a35
ritika bose <r3majumdar@yahoo.com>
2011-10-21 16:44:12 -0400

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # written page title basis of page
+  def title
+  	base_title = "Home Budget"
+  
+  	if @title.nil?
+  		base_title
+  		else
+  		 "#{base_title} | #{@title}" 
+  	end	
+  end	 	 
 end
